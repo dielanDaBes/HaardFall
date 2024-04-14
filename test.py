ae83e03d1c98a345e205354881bf7787f09204e4
dielanDaBes <dylanmoro@gmail.com>
2024-04-14 16:09:55 -0400

    Update test.py

--- test.py
+++ test.py
@@ -2,24 +2,6 @@
 import cv2
 import argparse
 
-# Define the parser
-parser = argparse.ArgumentParser(description='Haar Cascade detector with XML cascade file and png image to match against')
-
-# field, and using a default value if the argument 
-# isn't given
-parser.add_argument('--cascade', action="store", dest='cascade', default='test.xml')
-parser.add_argument('--inputImage', action="store", dest='inputImage', default='testFullMatch.PNG')
-parser.add_argument('--outputImage', action="store", dest='outputImage', default='CascadeMatchFull.jpg')
-parser.add_argument('--neighbors', action="store", dest='neighbors', default=5)
-
-# Now, parse the command line arguments and store the 
-# values in the `args` variable
-args = parser.parse_args()
- 
-print(args)
-# Read in the cascade classifiers for face and eyes
-test_cascade = cv2.CascadeClassifier(args.cascade)
-
 def detect_test(img):
      
     test_img = img.copy()    
@@ -28,9 +10,28 @@ def detect_test(img):
                                             minNeighbors = args.neighbors)    
     for (x, y, w, h) in test_rect:
         cv2.rectangle(test_img, (x, y), 
-                      (x + w, y + h), (255, 255, 255), 10)        
+                      (x + w, y + h), (255, 255, 255), args.lineSize)        
     return test_img
+ 
 
+# Define the parser
+parser = argparse.ArgumentParser(description='Haar Cascade detector with XML cascade file and png image to match against')
+
+# field, and using a default value if the argument 
+# isn't given
+parser.add_argument('--cascade', action="store", dest='cascade', default='test.xml')
+parser.add_argument('--inputImage', action="store", dest='inputImage', default='testInputFull.PNG')
+parser.add_argument('--outputImage', action="store", dest='outputImage', default='testOutputFull.jpg')
+parser.add_argument('--neighbors', action="store", dest='neighbors', default=5)
+parser.add_argument('--lineSize', action="store", dest='lineSize', default=2)
+
+# Now, parse the command line arguments and store the 
+# values in the `args` variable
+args = parser.parse_args()
+ 
+print(args)
+# Read in the cascade classifiers for face and eyes
+test_cascade = cv2.CascadeClassifier(args.cascade)
 
 img = cv2.imread(args.inputImage)
 test = detect_test(img)
