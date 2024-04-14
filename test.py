a3894d13f10aebbffddb3219b49fbdfe4e51d84e
dielanDaBes <dylanmoro@gmail.com>
2024-04-14 15:39:50 -0400

    Update test.py untested

--- test.py
+++ test.py
@@ -2,10 +2,27 @@
 import cv2
 import numpy as np
 import matplotlib.pyplot as plt #% matplotlib inline
+import argparse
+
+# Define the parser
+parser = argparse.ArgumentParser(description='Haar Cascade detector with XML cascade file and png image to match against')
+
+# field, and using a default value if the argument 
+# isn't given
+parser.add_argument('--cascade', action="store", dest='cascade', default='test.xml')
+parser.add_argument('--inputImage', action="store", dest='inputImage', default='testFullMatch.PNG')
+parser.add_argument('--outputImage', action="store", dest='outputImage', default='CascadeMatchFull.jpg')
+
+# Now, parse the command line arguments and store the 
+# values in the `args` variable
+args = parser.parse_args()
+
+# Individual arguments can be accessed as attributes...
+print (args.algo)
  
  
 # Read in the cascade classifiers for face and eyes
-test_cascade = cv2.CascadeClassifier('test.xml')
+test_cascade = cv2.CascadeClassifier(args.cascade)
 
 def detect_test(img):
      
@@ -19,7 +36,7 @@ def detect_test(img):
     return test_img
 
 
-img = cv2.imread('testMatchFull.PNG')
+img = cv2.imread(args.inputImage)
 test = detect_test(img)
 plt.imshow(test)
-cv2.imwrite('CascadeMatchFull.jpg', test)
+cv2.imwrite(args.outputImage, test)
